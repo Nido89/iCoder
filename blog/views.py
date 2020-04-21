f8afe48765672825eebaf5a946cf569c3207847f
Nido89 <syednaveedjalal@gmail.com>
2020-04-21 20:37:18 +0300

    Super User created in django admin and Added to url path

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def blogHome(request):
+    return render(request,'blog/blogHome.html')
+    #return HttpResponse('This is blogHome.We will keep all the BlogPost here.')
+
+def blogPost(request,slug):
+    return render(request,'blog/blogPost.html')
+    #return HttpResponse(f'This is blogPost:{slug}')
