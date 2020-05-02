1951ac20536d2f2291b1f4a0930db241993a5c1e
Nido89 <syednaveedjalal@gmail.com>
2020-05-02 12:48:33 +0300

    contact form with messages framework to display message tag added on contact form submission

--- home/views.py
+++ home/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render, HttpResponse
+from home.models import Contact
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
     return render(request,'home/home.html')
-    #return HttpResponse('This is Home')
+
 
 def about(request):
+    messages.info(request, 'Welcome1 to AboutUs')
+    messages.info(request, 'Welcome2 to AboutUs')
+    messages.info(request, 'Welcome3 to AboutUs')
     return render(request,'home/about.html')
 
 def contact(request):
+    if request.method=='POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        content = request.POST['content']
+        print(name,email,phone,content)
+
+        if len(name)<2 or len(email)<3 or len(phone)<10 or len(content)< 5:
+            messages.error(request,"Please fill the form correctly")
+        else:
+            contact = Contact(name=name, email=email, phone=phone,content=content)
+            contact.save()
+            messages.success(request,"Your message sent correctly")
     return render(request,'home/contact.html')
--- iCoder/settings.py
+++ iCoder/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -37,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home.apps.HomeConfig',
+    'blog.apps.BlogConfig'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MESSAGE_TAGS = {
+    messages.ERROR:'danger',
+}
