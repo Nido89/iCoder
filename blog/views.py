4eea88514b671c0012228db7fb10b9503b92b954
Nido89 <syednaveedjalal@gmail.com>
2020-05-11 22:12:57 +0300

    Blog Posts added with slug url

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render, HttpResponse
+from blog.models import Post
 
 # Create your views here.
 def blogHome(request):
-    return render(request,'blog/blogHome.html')
-    #return HttpResponse('This is blogHome.We will keep all the BlogPost here.')
+    allPosts = Post.objects.all()
+    #print(allPosts)
+    context = {'allPosts': allPosts}
+    return render(request,'blog/blogHome.html', context)
+
 
 def blogPost(request,slug):
-    return render(request,'blog/blogPost.html')
-    #return HttpResponse(f'This is blogPost:{slug}')
+    post = Post.objects.filter(slug=slug).first()
+    context = {'post': post}
+    return render(request,'blog/blogPost.html', context)
